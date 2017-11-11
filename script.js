554eed2052dd476038511e221acce717bad9b835
feifei <1026835233@qq,com>
2017-11-11 21:11:57 +0800

    添加一些交互以及触动

--- script.js
+++ script.js
@@ -1,22 +1,72 @@
-
     var myList = document.getElementById("banner_pic_list");
     var prev = document.getElementById("prev");
     var next = document.getElementById("next");
-
+    var button = document.getElementById('button').getElementsByTagName('span');
+    var index=1;
+    function showButton(){
+        for (var i = 0; i < button.length; i++) {
+            if (button[i].className == "on") {
+                button[i].className = "";
+                break;
+            }
+        }
+        button[index - 1].className = "on";
+    }
 
     function animation(offSet){
         var newLeft =  parseInt(myList.style.left)+offSet;
         myList.style.left = newLeft+"px";
         if(newLeft > -1920){
-            myList.style.left = -5760 + "px";
+            myList.style.left = -3840 + "px";
         }
-        if(newLeft < -5760){
+        if(newLeft < -3840){
             myList.style.left = -1920 + "px";
         }
     }
     next.onclick = function(){
+        if (index == 2) {
+            index = 1;
+        }
+        else{
+            index +=1;
+        }
+        showButton();
         animation(-1920);
     }
     prev.onclick = function(){
+        if (index == 1) {
+            index = 2;
+        }
+        else{
+            index -= 1;
+        }
+        showButton();
         animation(1920);
+    }
+    for (var i = 0; i < button.length; i++) {
+        button[i].onclick = function(){
+            if (this.className=="on") {
+                return;
+            }
+            var myIndex = parseInt(this.getAttribute('index'));
+            index = myIndex;
+            showButton();
+            if(index===1){
+                animation(-3840);
+            }
+            else if(index===2){
+                animation(-1920);
+            }
+        }
+    }
+
+    window.addEventListener('scroll',winScroll);
+    function winScroll(e){
+        var myScrollTop = pageYOffset;
+        var sideBar = document.getElementById("sideBar");
+        if (myScrollTop > 860) {
+            sideBar.style.display = "block";
+        }else{
+            sideBar.style.display = "none";
+        }
     }
